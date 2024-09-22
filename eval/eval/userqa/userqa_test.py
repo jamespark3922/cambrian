7813cc8b714754718b5d55509657bfb58fe06bec
jamespark3922 <jspark96@cs.washington.edu>
2024-09-21 21:54:15 -0700

    no save index

--- eval/eval/userqa/userqa_test.py
+++ eval/eval/userqa/userqa_test.py
@@ -16,7 +16,7 @@ def compute_metrics(jsonl_file, csv_file, extra_outdir=None):
             test_list.append(data)
             model = data.get("model_id", '')
     file_path = f"./answers/{model}_userqa_prediction.xlsx"
-    pd.DataFrame(test_list).to_excel(file_path)
+    pd.DataFrame(test_list).to_excel(file_path, index=False)
     # add_data_to_csv(csv_file, combined_data)
     print(f"prediction file: {file_path} is created.")
 
